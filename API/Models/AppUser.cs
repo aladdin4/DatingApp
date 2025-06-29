994ade29310aac482062f1a9001f69f9f3d01b79
A. Aladdin <aaladdin@finboa.com>
2025-06-29 22:37:44 +0300

    Add project files.

---
+++ API/Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Models;
+
+public class AppUser
+{
+    //has to be public so EF can utilize it. If private, EF won't be able to use it.
+
+    public int Id { get; set; }
+    public required string UserName { get; set; }
+}
---
+++ API/Program.cs
@@ -0,0 +1,22 @@
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            // This is the middleware section
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
